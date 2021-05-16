5b14d276d1416717a5ca0eff865aff5d46742db6
BhawickJain <bhawick@outlook.com>
2021-05-16 12:19:01 +0100

    fixed: #1 by spelling port as PORT to match case in env file

--- app/index.js
+++ app/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.port || 3000;
+// PORT as spelled in .env
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<hs>Hi There</h2>");
+  res.send("<hs>Hi There!!!!!</h2>");
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
